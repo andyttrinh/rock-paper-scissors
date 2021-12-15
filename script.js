5b2058880885c59a20c4abbc2e90fdae66b57a7d
Andy <andy.thienantrinh@gmail.com>
2021-12-15 15:13:12 -0500

    Added ability to play a round

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ let playerPlay = null;
 
 while (!choices.includes(playerPlay)) playerPlay = prompt('Enter "rock", "paper", or "scissors"');
 
+let playRound = (playerSelection, computerSelection) => {
+    const choices = [playerSelection, computerSelection];
+    if (playerSelection === computerSelection) {
+        console.log("It's a Tie!")
+    }
+    else if (choices.includes('rock') && choices.includes('paper')) {
+        if (playerSelection === 'paper') console.log("Player Wins!");
+        else console.log("PC Wins!");
+        console.log("Paper beats Rock.")
+    }
+    else if (choices.includes('rock') && choices.includes('scissors')) {
+        if (playerSelection === 'rock') console.log("Player Wins!");
+        else console.log("PC Wins!");
+        console.log("Rock beats Scissors.")
+    } else {
+        if (playerSelection === 'scissors') console.log("Player Wins!");
+        else console.log("PC Wins!");
+        console.log("Scissors beats Paper.")
+    }
+}
+
+playRound(playerPlay, computerPlay());
