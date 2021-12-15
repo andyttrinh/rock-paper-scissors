7d295c1ee4d36b2c04f7b900d3f7fa92c8ddf829
Andy <andy.thienantrinh@gmail.com>
2021-12-15 14:58:27 -0500

    Gave computer and player ability to choose

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("Hello")
+const choices = ['rock', 'paper', 'scissors'];
+
+let computerPlay = () => choices[Math.floor(Math.random() * (3 - 0) + 0)];
+let playerPlay = null;
+
+while (!choices.includes(playerPlay)) playerPlay = prompt('Enter "rock", "paper", or "scissors"');
+
