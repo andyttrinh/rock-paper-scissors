9ae0d77ddc38ca1add5eeba9f211d5f2e4966c5a
Andy <andy.thienantrinh@gmail.com>
2021-12-15 15:24:45 -0500

    Added ability to play mutliple rounds (game)

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
 const choices = ['rock', 'paper', 'scissors'];
 
 let computerPlay = () => choices[Math.floor(Math.random() * (3 - 0) + 0)];
-let playerPlay = null;
+let playerPlay = () => {
+    playerChoice = null;
+    while (!choices.includes(playerChoice)) playerChoice = prompt('Enter "rock", "paper", or "scissors"');
+    return playerChoice;
+}
 
-while (!choices.includes(playerPlay)) playerPlay = prompt('Enter "rock", "paper", or "scissors"');
+
 
 let playRound = (playerSelection, computerSelection) => {
     const choices = [playerSelection, computerSelection];
     if (playerSelection === computerSelection) {
-        console.log("It's a Tie!")
+        return null;
     }
     else if (choices.includes('rock') && choices.includes('paper')) {
-        if (playerSelection === 'paper') console.log("Player Wins!");
-        else console.log("PC Wins!");
-        console.log("Paper beats Rock.")
+        if (playerSelection === 'paper') return 1;
+        else return 0;
     }
     else if (choices.includes('rock') && choices.includes('scissors')) {
-        if (playerSelection === 'rock') console.log("Player Wins!");
-        else console.log("PC Wins!");
-        console.log("Rock beats Scissors.")
+        if (playerSelection === 'rock') return 1;
+        else return 0;
     } else {
-        if (playerSelection === 'scissors') console.log("Player Wins!");
-        else console.log("PC Wins!");
-        console.log("Scissors beats Paper.")
+        if (playerSelection === 'scissors') return 1;
+        else return 0;
     }
 }
 
-playRound(playerPlay, computerPlay());
+let game = () => {
+    let playerScore, computerScore;
+    playerScore = computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+       const res = playRound(playerPlay(), computerPlay);
+       if (res === 0) {
+           console.log("PC wins")
+           computerScore++;
+       }
+       else if (res === 1) {
+           console.log("Player wins")
+           playerScore++;
+       } else {
+           console.log("It's a tie")
+       }
+    }
+
+    if (playerScore > computerScore) {
+        console.log(`Player wins, ${playerScore} - ${computerScore}`);
+    }
+    else if (computerScore > playerScore) {
+        console.log(`Computer wins, ${computerScore} - ${playerScore}`);
+    } else {
+        console.log(`It is a tie, ${computerScore} - ${playerScore}`);
+    }
+}
+
+game();
